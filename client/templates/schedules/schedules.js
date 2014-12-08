0b3367deed118b2349659bcf27fb70ba15f6a253
wtfuii <klaus.deja@gmail.com>
2014-12-08 09:51:48 +0000

    Added edit-button in schedule list

--- client/templates/schedules/schedules.js
+++ client/templates/schedules/schedules.js
@@ -13,5 +13,24 @@ Template.schedules.helpers({
             groupedSched.push(f)
         }
         return groupedSched
+    },
+    
+    settings: function() {
+        return {
+            fields: [
+                {key: "year", label: "Jahr"},
+                {key: "month", label: "Monat"},
+                {key: "actions", label: "Aktionen", fn: function(value, object) {return new Spacebars.SafeString('<a href="#" class="edit">bearbeiten</a>')}}]
+        }
     }
+})
+
+Template.schedules.events ({
+ 'click .reactive-table tr': function(e) {
+    e.preventDefault();
+    if (e.target.className == "edit") {
+        Router.go("schedule", {_id: this._id})
+        
+    }
+ }
 })
