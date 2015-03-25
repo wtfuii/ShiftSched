61bebf78b6629db361fa449e6ac459ce28e77b60
wtfuii <klaus.deja@gmail.com>
2015-03-25 11:18:59 -0400

    fixed fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -12,7 +12,7 @@ if (Meteor.users.find().count() == 0) {
     Roles.addUsersToRoles(c, "2", "group")
     Roles.addUsersToRoles(d, "AG", "group")
     Meteor.call("onNewSchedule", {year: 2014, month: 2, group: "3"}, function (err, res) {
-        Times.insert({userid: a, year: 2014, month: 2, day: 4, group: 3, starttime: new Date(2014, 2, 4, 13), endtime: new Date(2014, 2, 4, 19)})
-        Times.insert({userid: a, year: 2014, month: 2, day: 6, group: 3, starttime: new Date(2014, 2, 6, 12), endtime: new Date(2014, 2, 6, 21)})
+        Times.insert({userid: a, year: 2014, month: 2, day: 4, group: 3, starttime: moment([2014, 2, 4, 13]).toISOString(), endtime: moment([2014, 2, 4, 19]).toISOString()})
+        Times.insert({userid: a, year: 2014, month: 2, day: 6, group: 3, starttime: moment([2014, 2, 6, 12]).toISOString(), endtime: moment([2014, 2, 6, 21]).toISOString()})
     })
 }
