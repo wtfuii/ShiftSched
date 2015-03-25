3860373422b1bdb3af8789734166f3bd746c7749
wtfuii <klaus.deja@gmail.com>
2015-03-25 11:18:08 -0400

    Advanced timeadding

--- client/templates/schedules/schedule.js
+++ client/templates/schedules/schedule.js
@@ -13,9 +13,24 @@ Template.schedule.helpers({
 Template.schedule.events({
     "click .deletetime": function() {
         Times.remove(this._id)
-    }, 
+    },
     "click .addtime": function(event) {
         event.preventDefault();
-        $(event.currentTarget).siblings(".addtimefield").append("<form><input type='text' name='start'><input type='text' name='end'></form>")
+        $(event.currentTarget).siblings(".addtimefield").append("<form><input type='text' name='start'><input type='text' name='end'><a href='#' class='submittime'><i class='fa fa-check'></i></a></form>")
+        $(event.currentTarget).remove()
+    },
+    "click .submittime": function(event) {
+        event.preventDefault()
+        var data = $(event.currentTarget).closest("td").data()
+        var starttime = $(event.currentTarget).siblings("[name=start]").val()
+        var endtime = $(event.currentTarget).siblings("[name=end]").val()
+        starttime = starttime.split(".")
+        endtime = endtime.split(".")
+        starttime = moment([data.year, data.month, data.day, starttime[0], starttime[1] || 0])
+        endtime = moment([data.year, data.month, data.day, endtime[0], endtime[1] || 0])
+        endtime = endtime <= starttime ? endtime.add(1, "d") : endtime
+        Times.insert({userid: data.user, month: data.month, year: data.year, day: data.day, starttime: starttime.toISOString(), endtime: endtime.toISOString()})
+        $(event.currentTarget).closest(".addtimefield").html("")
+        $(event.currentTarget).closest("td").append('<a href="#" class="addtime"><i class="fa fa-plus"></i></a>')
     }
 })
