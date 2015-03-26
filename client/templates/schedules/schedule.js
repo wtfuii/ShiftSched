785d47bb09405c805fae7e4b98530d68ab32e2e7
wtfuii <klaus.deja@gmail.com>
2015-03-26 08:44:46 -0400

    Displays total working hours

--- client/templates/schedules/schedule.js
+++ client/templates/schedules/schedule.js
@@ -7,6 +7,14 @@ Template.schedule.helpers({
            users[i].day = date
        }
        return users
+   },
+   getWorkedHours: function(month, year, id) {
+       var t = Times.find({month: month, year: year, userid: id}).fetch()
+       r = 0
+       for (i = 0; i < t.length; i++) {
+           r = r + t[i].workedhours
+       }
+       return moment.duration(r).asHours()
    }
 })
 
@@ -29,8 +37,9 @@ Template.schedule.events({
         starttime = moment([data.year, data.month, data.day, starttime[0], starttime[1] || 0])
         endtime = moment([data.year, data.month, data.day, endtime[0], endtime[1] || 0])
         endtime = endtime <= starttime ? endtime.add(1, "d") : endtime
-        Times.insert({userid: data.user, month: data.month, year: data.year, day: data.day, starttime: starttime.toISOString(), endtime: endtime.toISOString()})
+        Times.insert({userid: data.user, month: data.month, year: data.year, day: data.day, starttime: starttime.toISOString(), endtime: endtime.toISOString(), workedhours: endtime.diff(starttime)})
+        var td = $(event.currentTarget).closest("td")
         $(event.currentTarget).closest(".addtimefield").html("")
-        $(event.currentTarget).closest("td").append('<a href="#" class="addtime"><i class="fa fa-plus"></i></a>')
+        td.append('<a href="#" class="addtime"><i class="fa fa-plus"></i></a>')
     }
 })
--- server/fixtures.js
+++ server/fixtures.js
@@ -3,7 +3,7 @@ if (Meteor.users.find().count() == 0) {
     var b = Accounts.createUser({username: "HMeier", password:"123456", email:"dafuqington@gmail.com"})
     var c = Accounts.createUser({username: "PSchlau", password:"123456", email:"oh@hai.de"})
     var d = Accounts.createUser({username: "BMaja", password:"123456", email:"biene@maja.net"})
-    Meteor.users.update({username : "ESchoeneberg"}, {$set: {hours: 39}})
+    Meteor.users.update({username : "EPutin"}, {$set: {hours: 39}})
     Meteor.users.update({username : "HMeier"}, {$set: {hours: 38}})
     Meteor.users.update({username : "PSchlau"}, {$set: {hours: 38}})
     Meteor.users.update({username : "BMaja"}, {$set: {hours: 38}})
